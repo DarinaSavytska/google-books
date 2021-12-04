426f6d6e124dcf5e8df7ca30b8b72ddb88e6ba44
Darina Savytska <coldshine99@gmail.com>
2021-12-04 16:22:21 +0200

    Add sort and filter

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,23 @@ export const App: React.FC = () => {
   const [totalBook, setTotalBook] = useState<string | number>('')
   const [allBook, setAllBook] = useState<Book[]>([]);
   const [searchBook, setSearchBook] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [sortBy, setSortBy] = useState<string>('relevance');
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchBook(event.target.value);
   };
 
+  const handleSelectedFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const handleSelectedSort = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortBy(event.target.value);
+  };
+
   const getBook = async () => {
-    const found = await getBooks(searchBook);
+    const found = await getBooks(searchBook, sortBy, selectedCategory);
 
     setAllBook(found.items);
     setTotalBook(found.totalItems)
@@ -38,6 +48,31 @@ export const App: React.FC = () => {
             value={searchBook}
             onChange={handleInput}
           />
+
+          Categories
+          <select
+            value={selectedCategory}
+            onChange={handleSelectedFilter}
+            className="App__form--item"
+          >
+            <option value="">All</option>
+            <option value="art">Art</option>
+            <option value="biography">Biography</option>
+            <option value="computers">Computers</option>
+            <option value="history">History</option>
+            <option value="medical">Medical</option>
+            <option value="poetry">Poetry</option>
+          </select>
+
+          Sorting by
+          <select
+            value={sortBy}
+            onChange={handleSelectedSort}
+            className="App__form--item"
+          >
+            <option value="relevance">Relevance</option>
+            <option value="newest">Newest</option>
+          </select>
 
           <button
             className="App__form--item"
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,9 @@
-export async function getBooks(title: string) {
+export async function getBooks(title: string, sort: string, category: string) {
   // добавить обработку ошибок
-  const books = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${title}`)
+
+  // const key = 'AIzaSyBBA37PHb5UjrZfGBvNsHUF8xct7TRybJ8';
+
+  const books = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${title}+subject:${category}&orderBy=${sort}&maxResults=30`)
     .then(res => res.json())
 
   return books;
